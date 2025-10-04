fc5d06e309f8ff5e3129da75daae989dd06764c5
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-04 16:35:37 +1000

    icon images imported into assets/images

--- frontend/KTMVTour/app/(tabs)/_layout.tsx
+++ frontend/KTMVTour/app/(tabs)/_layout.tsx
@@ -1,44 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
 
 const layout = () => {
   return (
     <Tabs>
       {/* Making sure all the top navbar route sections created by native are hidden */}
       <Tabs.Screen
-      name='index'
-      options={{
-        title:"Home",
-        headerShown:false
-      }}
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
       />
 
       <Tabs.Screen
-      name='tours'
-      options={{
-        title:"Tours",
-        headerShown:false
-      }}
+        name="tours"
+        options={{
+          title: "Tours",
+          headerShown: false,
+        }}
       />
 
       <Tabs.Screen
-      name='community'
-      options={{
-        title:"Community",
-        headerShown:false
-      }}
+        name="community"
+        options={{
+          title: "Community",
+          headerShown: false,
+        }}
       />
 
       <Tabs.Screen
-      name='profile'
-      options={{
-        title:"Profile",
-        headerShown:false
-      }}
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+        }}
       />
     </Tabs>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
