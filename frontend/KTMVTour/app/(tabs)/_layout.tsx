9ba660868c447116ebd4aee9ec1d7efdbac81427
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-04 15:44:25 +1000

    added tabs

---
+++ frontend/KTMVTour/app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const layout = () => {
+  return (
+    <Tabs>
+      {/* Making sure all the top navbar route sections created by native are hidden */}
+      <Tabs.Screen
+      name='index'
+      options={{
+        title:"Home",
+        headerShown:false
+      }}
+      />
+
+      <Tabs.Screen
+      name='tours'
+      options={{
+        title:"Tours",
+        headerShown:false
+      }}
+      />
+
+      <Tabs.Screen
+      name='community'
+      options={{
+        title:"Community",
+        headerShown:false
+      }}
+      />
+
+      <Tabs.Screen
+      name='profile'
+      options={{
+        title:"Profile",
+        headerShown:false
+      }}
+      />
+    </Tabs>
+  )
+}
+
+export default layout
