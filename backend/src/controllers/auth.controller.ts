91ffd5d731bb6813eb9fa600f0bf22038b177b01
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-10 21:23:39 +1100

    simple register function created

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,31 @@
-import { Request,Response,NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import CustomError from "../middlewares/error-handler.middleware";
-
+import User from "../models/user.model";
 //register/signup function:
+const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password, username, profilePicture } = req.body;
+
+    if(!email){
+      throw new CustomError(`Email required`,400)
+    }
+    if(!password){
+      throw new CustomError(`Password required`,400)
+    }
+    if(!username){
+      throw new CustomError(`Username required`,400)
+    }
+    
+    const user = await User.create(email,password,username,profilePicture)
+
+    res.status(201).json({
+      message: `User registered`,
+      data:user
+    })
+  } catch (err) {
+    next(err);
+  }
+};
 
 // login function:
 
