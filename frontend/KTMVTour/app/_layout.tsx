6cd545a1c9968e0b410778f78bbb8329d409f695
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-16 10:52:44 +1100

    login page protected route added

--- frontend/KTMVTour/app/_layout.tsx
+++ frontend/KTMVTour/app/_layout.tsx
@@ -4,13 +4,19 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a client
 const queryClient = new QueryClient();
+const isAuthenticated = false
 
 export default function RootLayout() {
   return (
     //wrapping out app with the queryClientProvider
     <QueryClientProvider client={queryClient}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack >
+        <Stack.Protected guard={isAuthenticated}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack.Protected>
+        <Stack.Protected guard={!isAuthenticated}>
+          <Stack.Screen name="login" options={{headerShown: false}}/>
+        </Stack.Protected>
       </Stack>
     </QueryClientProvider>
   );
