27d5450878c749503b6a817f280416f22fdc352d
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-17 20:32:34 +1100

    accessed user properties on profile page + deleting mmkv items on logout

--- frontend/KTMVTour/app/(tabs)/profile.tsx
+++ frontend/KTMVTour/app/(tabs)/profile.tsx
@@ -1,20 +1,42 @@
-import { View, Text, ImageBackground, Button } from 'react-native'
-import React from 'react'
-import { useAuthStore } from '@/src/store/auth.store'
+import { View, Text, ImageBackground, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useAuthStore } from "@/src/store/auth.store";
+import { MMKV } from "react-native-mmkv";
 
-
-const profile = () => {
-  const {logout} = useAuthStore()
-
-  const handlelogout = ()=>{
-    logout()
-  }
-  return (
-    <View className='flex-1 items-center bg-black'>
-      <Text className='text-4xl font-bold text-white mt-14'>Profile</Text>
-      <Button title='logout' onPress={handlelogout}/>
-    </View>
-  )
+interface User {
+  username: string;
+  id: string;
+  email: string;
 }
 
-export default profile
+const profile = () => {
+  const { logout } = useAuthStore();
+  const [user, setUser] = useState<User | null>(null);
+  const storage = new MMKV();
+
+  useEffect(() => {
+    const storedUser = storage.getString("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const handlelogout = () => {
+    storage.delete('user')
+    storage.delete('KTMVTour_token')
+    logout()
+    // console.log(user)
+  };
+
+  return (
+    <View className="flex-1 items-center bg-black">
+      <Text className="text-4xl font-bold text-white mt-14">Profile</Text>
+      <Text className="text-4xl font-bold text-white mt-2 mb-14">
+        Hi {user?.username ?? "guest"}
+      </Text>
+      <Button title="logout" onPress={handlelogout} />
+    </View>
+  );
+};
+
+export default profile;
