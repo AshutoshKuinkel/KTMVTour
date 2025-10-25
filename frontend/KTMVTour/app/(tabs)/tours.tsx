26b627887f67838d0d260855d34690c54d4cd49c
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-25 21:14:57 +1100

    RN Vision Camera setup

--- frontend/KTMVTour/app/(tabs)/tours.tsx
+++ frontend/KTMVTour/app/(tabs)/tours.tsx
@@ -1,6 +1,6 @@
-import { View, Text } from "react-native";
-import React from "react";
-import{
+import { View, Text, ActivityIndicator } from "react-native";
+import React, { useEffect } from "react";
+import {
   Camera,
   useCameraDevice,
   useCameraPermission,
@@ -9,18 +9,55 @@ import{
 const tours = () => {
   const device = useCameraDevice("back");
 
-  const { hasPermission } = useCameraPermission();
+  const { hasPermission, requestPermission } = useCameraPermission();
 
-  if (!hasPermission)
-    return console.log(`Permission not obtained to use RN Vision Camera`);
-  if (device == null) return console.log(`Can't find camera on device.`);
+  //useEffect hook in React is a built-in hook that allows functional components to perform "side effects."
+  //Side effects are operations that interact with the outside world or affect things beyond the component's direct rendering, such as:
+  //Data fetching
+  //DOM manipulation
+  //Subscriptions
+  //Timers
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission]);
+
+
+  if (hasPermission == null) {
+    return (
+      <View className="flex items-center justify-center h-screen bg-black">
+        <ActivityIndicator size={"large"} color={"#8B5CF6"} />
+      </View>
+    );
+  }
+
+  if (!hasPermission) {
+    return (
+      <View className="flex-1 items-center pt-8 bg-black h-screen">
+        <Text className="text-xl font-bold text-white mt-14 text-center">
+          Camera permission is required. Please enable it in settings.
+        </Text>
+      </View>
+    );
+  }
+
+  if (device == null) {
+    return (
+      <View className="flex-1 items-center pt-8 bg-black h-screen">
+        <Text className="text-xl font-bold text-white mt-14 text-center">
+          No camera device found
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <View className="flex-1 items-center pt-8 bg-black">
       <Text className="text-4xl font-bold text-white mt-14">Tours</Text>
-      <Camera device={device} isActive={true} />
+      <Camera device={device} isActive={true}/>
     </View>
-  ); 
+  );
 };
 
 export default tours;
