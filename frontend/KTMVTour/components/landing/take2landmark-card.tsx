d79f0908d4478a54eb24f804f039c35b62bfc251
KBDizzee <ashutoshkuinkel42@gmail.com>
2025-10-25 11:53:21 +1100

    take me to landmark map linking setup. Add pressable to call function on press

--- frontend/KTMVTour/components/landing/take2landmark-card.tsx
+++ frontend/KTMVTour/components/landing/take2landmark-card.tsx
@@ -1,42 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { MapPin, Navigation } from 'lucide-react-native'
+import { View, Text, Linking, Platform } from "react-native";
+import React from "react";
+import { MapPin, Navigation } from "lucide-react-native";
+import Location from "expo-location";
 
+interface openMapArgs {
+  latitude: string | number;
+  longitude: string | number;
+  label: "Landmarks near me";
+}
 const TakeToLandmark = () => {
+  const openMap = ({ latitude, longitude, label }: openMapArgs) => {
+    const scheme = Platform.select({
+      ios: `maps:?q=${label}&ll=${latitude},${longitude}`,
+      android: `geo:${latitude},${longitude}?q=${latitude},${longitude}(${label})`,
+    });
+
+    if (scheme) {
+      Linking.openURL(scheme).catch((err) =>
+        console.error("Error opening map: ", err)
+      );
+    }
+  };
+
+  const handleOpenMap = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission denied");
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({})
+
+    openMap({
+      latitude:location.coords.latitude,
+      longitude:location.coords.longitude,
+      label:'Landmarks near me'
+    })
+  };
   return (
     <View className="bg-card mt-12 w-[90vw] rounded-2xl border border-border flex items-center shadow-xl">
-          <View className="flex flex-col items-center justify-center">
-            {/* Nav logo */}
-            <View className="bg-[#2C1F48] p-5 rounded-full mt-5">
-              <Navigation color={"#8B5CF6"} size={40} />
-            </View>
+      <View className="flex flex-col items-center justify-center">
+        {/* Nav logo */}
+        <View className="bg-[#2C1F48] p-5 rounded-full mt-5">
+          <Navigation color={"#8B5CF6"} size={40} />
+        </View>
 
-            {/* Text */}
-            <View className="mt-4">
-              <Text className="text-white text-2xl font-semibold text-center">
-                Take me to a nearby landmark
-              </Text>
-            </View>
+        {/* Text */}
+        <View className="mt-4">
+          <Text className="text-white text-2xl font-semibold text-center">
+            Take me to a nearby landmark
+          </Text>
+        </View>
 
-            {/* Text */}
-            <View className="mt-4">
-              <Text className="text-secondary text-md text-center max-w-[80vw] font-semibold">
-                Discover the closest historical and cultural sites around you
-              </Text>
-            </View>
+        {/* Text */}
+        <View className="mt-4">
+          <Text className="text-secondary text-md text-center max-w-[80vw] font-semibold">
+            Discover the closest historical and cultural sites around you
+          </Text>
+        </View>
 
-            {/* Find nearby landmarks button */}
-            <View className="mt-6 w-[80vw] pb-7">
-              <View className="flex-row items-center bg-button justify-center rounded-lg py-3 hover:cursor-pointer gap-3">
-                <MapPin size={20} color={"white"} />
-                <Text className="text-white text-lg font-semibold">
-                  Find Nearby Landmarks
-                </Text>
-              </View>
-            </View>
+        {/* Find nearby landmarks button */}
+        <View className="mt-6 w-[80vw] pb-7">
+          <View className="flex-row items-center bg-button justify-center rounded-lg py-3 hover:cursor-pointer gap-3"> 
+            {/* Convert this into a pressable next so we can call function on press */}
+            <MapPin size={20} color={"white"} />
+            <Text className="text-white text-lg font-semibold">
+              Find Nearby Landmarks
+            </Text>
           </View>
         </View>
-  )
-}
+      </View>
+    </View>
+  );
+};
 
-export default TakeToLandmark
+export default TakeToLandmark;
