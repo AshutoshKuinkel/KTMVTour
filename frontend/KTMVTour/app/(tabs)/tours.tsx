2a10d0f54103626629b9b49ad209a73cf6d1f76b
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-25 14:01:13 +1100

    RN Vision camera successfully setup. To Do: enable permissions

--- frontend/KTMVTour/app/(tabs)/tours.tsx
+++ frontend/KTMVTour/app/(tabs)/tours.tsx
@@ -1,12 +1,24 @@
 import { View, Text } from "react-native";
 import React from "react";
-import Camera from "react-native-vision-camera"
+import{
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from "react-native-vision-camera";
 
 const tours = () => {
+  const device = useCameraDevice("back");
+
+  const { hasPermission } = useCameraPermission();
+
+  if (!hasPermission)
+    return console.log(`Permission not obtained to use RN Vision Camera`);
+  if (device == null) return console.log(`Can't find camera on device.`);
 
   return (
     <View className="flex-1 items-center pt-8 bg-black">
       <Text className="text-4xl font-bold text-white mt-14">Tours</Text>
+      <Camera device={device} isActive={true} />
     </View>
   );
 };
