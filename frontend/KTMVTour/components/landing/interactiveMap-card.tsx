d66a2b55ddf39b4787280e24fdcaec05c31d2b9e
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-10-26 20:03:01 +1100

    RN maps installed. To do: see how to implement RN maps

--- frontend/KTMVTour/components/landing/interactiveMap-card.tsx
+++ frontend/KTMVTour/components/landing/interactiveMap-card.tsx
@@ -1,45 +1,55 @@
 import { Map, MapPin } from "lucide-react-native";
 import { View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import MapView from "react-native-maps";
+import { useState } from "react";
 
 const InteractiveMapCard = () => {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <View className="bg-post w-[90vw] mt-12 rounded-2xl border border-border pb-5 overflow-hidden">
       {/* Image of card for loading state */}
-      <LinearGradient
-        colors={["#1f1f1f", "rgba(45, 27, 105, 0.2)"]}
-        className=" w-full bg-gradient-to-br from-[#1f1f1f] to-[#2d1b69]/20 h-[200px]  flex items-center justify-center rounded-t-2xl"
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 0 }}
-      >
-        <View className="flex items-center justify-center h-[230px] w-full">
-          <View className="flex flex-col items-center justify-center">
-            <Map size={60} color={"#8B5CF6"}/>
-            <Text className="text-secondary animate-pulse">
-              Interactive map loading...
-            </Text>
+      { isLoading ?
+        <LinearGradient
+          colors={["#1f1f1f", "rgba(45, 27, 105, 0.2)"]}
+          className=" w-full bg-gradient-to-br from-[#1f1f1f] to-[#2d1b69]/20 h-[200px]  flex items-center justify-center rounded-t-2xl"
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        >
+          <View className="flex items-center justify-center h-[230px] w-full">
+            <View className="flex flex-col items-center justify-center">
+              <Map size={60} color={"#8B5CF6"} />
+              <Text className="text-secondary animate-pulse">
+                Interactive map loading...
+              </Text>
+            </View>
+            <View className="absolute top-12 left-12 animate-pulse">
+              <MapPin size={24} color={"#8B5CF6"} />
+            </View>
+            <View className="absolute bottom-12 left-8 animate-pulse">
+              <MapPin size={24} color={"#8B5CF6"} />
+            </View>
+            <View className="absolute top-8 right-8 animate-pulse">
+              <MapPin size={24} color={"#8B5CF6"} />
+            </View>
           </View>
-          <View className="absolute top-12 left-12 animate-pulse">
-            <MapPin size={24} color={"#8B5CF6"} />
-          </View>
-          <View className="absolute bottom-12 left-8 animate-pulse">
-            <MapPin size={24} color={"#8B5CF6"} />
-          </View>
-          <View className="absolute top-8 right-8 animate-pulse">
-            <MapPin size={24} color={"#8B5CF6"} />
-          </View>
+        </LinearGradient> :
+        <View className="w-full h-[200px]  flex items-center justify-center rounded-t-2xl">
+          <MapView style={{height:200}}/>
         </View>
-      </LinearGradient>
+      }
 
       {/* Listing nearby landmarks with distance */}
       <View className="mt-6">
-        <Text className="text-white font-semibold pl-4 text-xl">Nearby Landmarks</Text>
+        <Text className="text-white font-semibold pl-4 text-xl">
+          Nearby Landmarks
+        </Text>
 
         <View className="p-2 mt-2">
           {/* First landmark */}
           <View className="flex-row justify-between items-center">
             <View className="flex flex-row gap-1 items-center">
-              <MapPin size={14} color={'#8B5CF6'}/>
+              <MapPin size={14} color={"#8B5CF6"} />
               <Text className="text-white text-lg">Swayambhunath Temple</Text>
             </View>
             <Text className="text-secondary text-lg text-center">2.1km NW</Text>
@@ -48,7 +58,7 @@ const InteractiveMapCard = () => {
           {/* Second landmark */}
           <View className="flex-row justify-between items-center mt-3">
             <View className="flex flex-row gap-1 items-center">
-              <MapPin size={14} color={'#8B5CF6'}/>
+              <MapPin size={14} color={"#8B5CF6"} />
               <Text className="text-white text-lg">Durbar Square</Text>
             </View>
             <Text className="text-secondary text-lg text-center">1.8km S</Text>
@@ -57,13 +67,14 @@ const InteractiveMapCard = () => {
           {/* Third landmark */}
           <View className="flex-row justify-between items-center mt-3">
             <View className="flex flex-row gap-1 items-center">
-              <MapPin size={14} color={'#8B5CF6'}/>
+              <MapPin size={14} color={"#8B5CF6"} />
               <Text className="text-white text-lg">Boudhanath Stupa</Text>
             </View>
-            <Text className="text-secondary text-lg text-center">3.5 km NE</Text>
+            <Text className="text-secondary text-lg text-center">
+              3.5 km NE
+            </Text>
           </View>
         </View>
-
       </View>
     </View>
   );
