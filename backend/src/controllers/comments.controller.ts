fc3b98470202fef65728fc77e44c5e81fcc76f96
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-11-27 08:41:01 +1100

    feat: Comments cached for faster loading times

--- backend/src/controllers/comments.controller.ts
+++ backend/src/controllers/comments.controller.ts
@@ -3,6 +3,7 @@ import CustomError from "../middlewares/error-handler.middleware";
 import { getCommentModel, postCommentModel } from "../models/comments.model";
 import { postgresPool } from "../config/db.config";
 import { getPagination } from "../utils/pagination.utils";
+import redisClient from "../config/redisClient.config";
 
 export const commentOnPost = async (
   req: Request,
@@ -43,26 +44,53 @@ export const getAllComments = async (
   next: NextFunction
 ) => {
   try {
-    const {postId} = req.params
+    const { postId } = req.params;
     const { currentPage } = req.query;
     const page = Number(currentPage) || 1;
-    const limit = 20
+    const limit = 20;
+    const cachedKey = `comments:all:page:${page}`;
+    let cachedComments;
+    try {
+      cachedComments = await redisClient.get(cachedKey);
+    } catch (err: any) {
+      console.error(`Redis get err:`, err);
+    }
+
+    if(cachedComments){
+      return res.status(200).json({
+        message:`Comments fetched from redis`,
+        data:JSON.parse(cachedComments).data,
+        pagination: JSON.parse(cachedComments).pagination,
+      })
+    }
 
     // total count of comments:
-    const totalQuery = await postgresPool.query(`SELECT COUNT(*) FROM comments WHERE post_id = $1`,[postId]);
+    const totalQuery = await postgresPool.query(
+      `SELECT COUNT(*) FROM comments WHERE post_id = $1`,
+      [postId]
+    );
     const total = Number(totalQuery.rows[0].count);
 
-    const comments = await getCommentModel(page,limit,postId)
+    const comments = await getCommentModel(page, limit, postId);
 
-    const pagination = getPagination(page,limit,total)
+    const pagination = getPagination(page, limit, total);
+
+    const responseData = {
+      data:comments,
+      pagination
+    }
+
+    try{
+      await redisClient.set(cachedKey,JSON.stringify(responseData),{EX: 1800})
+    }catch(err:any){
+      console.error(`Redis set err:`,err)
+    }
 
     // response:
     res.status(200).json({
       message: `Comments fetched for post: ${postId}`,
-      data: comments,
-      pagination
+      ...responseData
     });
-
   } catch (err: any) {
     next(err);
   }
