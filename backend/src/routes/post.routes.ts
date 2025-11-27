c7a0ccd79f55c3fd496476e0fa40e5d3941d109a
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-11-27 15:59:02 +1100

    feat: rate-limit within specific API endpoints

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -1,3 +1,4 @@
+import { limiter } from './../middlewares/rate-limit.middleware';
 import express from "express";
 import { uploader } from "../middlewares/uploader.middleware";
 import { authenticate } from "../middlewares/auth.middleware";
@@ -6,7 +7,7 @@ import { createPost } from "../controllers/posts.controller";
 const router = express.Router();
 const upload = uploader()
 
-router.post('/postPics',authenticate(),upload.array('photos',10), createPost)
+router.post('/postPics',authenticate(),limiter,upload.array('photos',10), createPost)
 
 
 export default router
