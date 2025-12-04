bf8b527f2ee13ca62b131ecee7bc016573da5f56
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-12-04 11:40:09 +1100

    To do: invalidate cache logic backend

--- frontend/KTMVTour/src/api/comments.api.ts
+++ frontend/KTMVTour/src/api/comments.api.ts
@@ -4,7 +4,6 @@ import api from './index'
 export const getAllCommentsAPI = async(postId:string,pageParam:number)=>{
   try{
     const response = await api.get(`/social/allComments/${postId}?currentPage=${pageParam}`)
-    console.log(response.data)
     return response.data
   }catch(err:any){
     return err.response.data
