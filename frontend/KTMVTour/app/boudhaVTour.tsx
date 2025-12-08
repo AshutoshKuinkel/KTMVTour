f65b4380ebd48e6c2768482b1d180f3708354b54
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2025-12-09 10:04:54 +1100

    ...

--- frontend/KTMVTour/app/boudhaVTour.tsx
+++ frontend/KTMVTour/app/boudhaVTour.tsx
@@ -2,11 +2,12 @@ import { View, Text } from "react-native";
 import React from "react";
 import Video from "react-native-video";
 import { router } from "expo-router";
+import UnityView from '@azesmway/react-native-unity'
 
 const boudhaVTour = () => {
   return (
     <View style={{ height: "100%", width: "100%" }}>
-      <Video
+      {/* <Video
         source={require("../assets/vid/vtour-vid1.mp4")}
         style={{
           height: "100%",
@@ -21,6 +22,15 @@ const boudhaVTour = () => {
         paused={false}
         onEnd={()=>{
           router.push('/tours')
+        }}
+      /> */}
+      <UnityView
+        style={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          top: 1,
+          bottom: 1
         }}
       />
     </View>
